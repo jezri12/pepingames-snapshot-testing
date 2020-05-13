d0b7b0f417c8286a449968adb494ab608a396c50
jezri12 <51058097+jezri12@users.noreply.github.com>
2020-05-13 18:07:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function enterlatestsgame(){
+    let passconfirm = prompt("PASSWORD:","")
+    if(passconfirm == "pepingames for life"){
+        alert("link :  pepingames-snapshot-testing.glitch.me/indexgame.html")
+    }else{
+        alert("WRONG!!!!! ):<")
+    }
+}
